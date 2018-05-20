0437e0018940e31652863b4a9548ebb1df8788f3
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2018-05-20 13:11:48 +0530

    vision 34

--- mains_2017 (1).mm
+++ mains_2017 (1).mm
@@ -1,7 +1,7 @@
 <map version="freeplane 1.5.9">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="UPSC" LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_523629448" CREATED="1487759666321" MODIFIED="1489498611522" MAX_WIDTH="82.21302670596266 pt" MIN_WIDTH="82.21302670596266 pt"><hook NAME="MapStyle" background="#f7f7f7">
-    <properties show_icon_for_attributes="true" fit_to_viewport="false;" show_note_icons="true" edgeColorConfiguration="#808080ff,#a3a3ffff,#ff77b3ff,#ff9933ff,#9999ffff,#968900ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff"/>
+    <properties fit_to_viewport="false;" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#a3a3ffff,#ff77b3ff,#ff9933ff,#9999ffff,#968900ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt" TEXT_SHORTENED="true">
@@ -2517,7 +2517,7 @@
 <node TEXT="Global Atmosphere Watch" ID="ID_818541867" CREATED="1524824251210" MODIFIED="1524824257450"/>
 <node TEXT="Prakriti Khoj" ID="ID_709425086" CREATED="1524826789236" MODIFIED="1524826798047"/>
 </node>
-<node TEXT="International" FOLDED="true" ID="ID_764045234" CREATED="1521106804529" MODIFIED="1521106807217">
+<node TEXT="International" ID="ID_764045234" CREATED="1521106804529" MODIFIED="1521106807217">
 <node TEXT="South Asia Growth Quadrangle" ID="ID_1220507495" CREATED="1521106808436" MODIFIED="1521106828267">
 <node TEXT="BBIN 1997" ID="ID_1603575360" CREATED="1521115275200" MODIFIED="1521115278638"/>
 </node>
