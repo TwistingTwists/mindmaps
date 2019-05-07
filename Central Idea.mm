4ef5c33a67fd1cdbf58a84ed8e82ec7e97ae0323
Abhishek <abhishek.tripathi456@gmail.com>
2019-05-08 00:08:16 +0530

    gcnfg

---
+++ Central Idea.mm
@@ -0,0 +1,159 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Baba 60 days current affair s" FOLDED="false" ID="ID_191153586" CREATED="1557163800507" MODIFIED="1557252615834" ICON_SIZE="36.0 pt" LINK="menuitem:_ExternalImageAddAction" STYLE="oval">
+<font SIZE="22"/>
+<hook NAME="MapStyle" zoom="0.5">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64.0 pt" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Segoe Print" SIZE="22"/>
+<edge COLOR="#ffffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32.0 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="18" BOLD="false" ITALIC="true"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24.0 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="13"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24.0 px" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="12"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16.0 px">
+<font SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="20" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge COLOR="#ffffff"/>
+<node TEXT="PM surakshit matritva Abhiyaan ( PMSMA )" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1765845329" CREATED="1494905259179" MODIFIED="1557252774586">
+<edge COLOR="#00007c"/>
+<node TEXT="MoHFW" ID="ID_1334492784" CREATED="1494905277119" MODIFIED="1526092893761">
+<font BOLD="true"/>
+</node>
+<node TEXT="9th of every month" ID="ID_1199438655" CREATED="1494905268052" MODIFIED="1494905272828">
+<node TEXT="at least one antenatal checkup for all pregnant women in their second or third trimester by a physician/specialist" ID="ID_466200300" CREATED="1557252661982" MODIFIED="1557252661982"/>
+</node>
+<node TEXT="s identification and follow up of high risk pregnancies." FOLDED="true" ID="ID_71750523" CREATED="1557252694552" MODIFIED="1557252695149">
+<node TEXT="Green Sticker" FOLDED="true" ID="ID_492150853" CREATED="1557252701319" MODIFIED="1557252706501">
+<node TEXT="no risk factor detected" ID="ID_1101302266" CREATED="1557252707236" MODIFIED="1557252707236"/>
+</node>
+<node TEXT="Red Sticker" FOLDED="true" ID="ID_1875307427" CREATED="1557252711056" MODIFIED="1557252711319">
+<node TEXT="&#x2013; for women with high risk pregnancy" ID="ID_1789261686" CREATED="1557252717912" MODIFIED="1557252718215"/>
+</node>
+</node>
+<node TEXT="Green, red stickers" ID="ID_453480442" CREATED="1494905285784" MODIFIED="1494905291747"/>
+<node TEXT="use private doctors (voluntary services ) if sarkari not available" ID="ID_1674850421" CREATED="1497260491452" MODIFIED="1557252745936"/>
+</node>
+<node TEXT="atal bhujal" POSITION="left" ID="ID_1131939472" CREATED="1522214551864" MODIFIED="1557253979315">
+<edge COLOR="#007c00"/>
+<node TEXT="demand side address" ID="ID_1426893736" CREATED="1525526007192" MODIFIED="1525526011709"/>
+<node TEXT="50% fund to gram panchayat + rest to state for strengthening institutional arrangement for water mgmt" ID="ID_716902053" CREATED="1548353044200" MODIFIED="1548353071703"/>
+<node ID="ID_1328155786" CREATED="1525526012334" MODIFIED="1554443268515"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0066">WB loan </font>+ central govt fund (Min water)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="sustainable groundwater mgmt via community participation&#xa;&#xa;in priority districts" ID="ID_1255670894" CREATED="1522214554978" MODIFIED="1554443279956">
+<node ID="ID_574717537" CREATED="1548353198118" MODIFIED="1554443293328"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0066">water budgeting</font>,
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      gram panchayat based
+    </p>
+    <p>
+      &#160;water security plans
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="behavioral changes&#xa;convergence of schemes" ID="ID_1731844819" CREATED="1548353218972" MODIFIED="1548353231171"/>
+</node>
+</node>
+<node TEXT="Island development Agency (IDA)" POSITION="right" ID="ID_32118052" CREATED="1501201101846" MODIFIED="1557254010340">
+<edge COLOR="#7c007c"/>
+<node TEXT="Home Min = chair&#xa;5 andaman, 4 Lakshadweep" ID="ID_1370217387" CREATED="1525237959206" MODIFIED="1525237975443"/>
+</node>
+</node>
+</map>
